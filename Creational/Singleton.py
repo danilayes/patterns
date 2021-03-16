2f2dd239a33a74f4c80dc02ce98084f5b8502a9e
Danila <user@macs-Danila.local>
2021-03-16 13:17:04 +0200

    singleton

---
+++ Creational/Singleton.py
@@ -0,0 +1,24 @@
+class LogInTemp:
+
+    instance = None
+
+    def __new(cls, *args, **kwargs):
+        if not isinstance(cls.instance,cls):
+            cls.__instance = super(LogInTemp,cls).__new(cls)
+        else:
+            print("Object of LogIn is already created")
+        return cls.instance
+
+    def log(self):
+        log_in = input("Enter login: ")
+        password = input("Enter password: ")
+        print(f"You logged in like {log_in}")
+
+
+
+
+if __name == "main":
+    login = LogInTemp()
+    login.log()
+    login2 = LogInTemp()
+    login2.log()
